f6351b68a64f7cb676b1e852b46f7e32d0f682cc
Han Xiao <artex.xh@gmail.com>
2017-06-04 02:06:50 +0200

    forwarding self-intro to group chat

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ import itchat
 from itchat.content import TEXT, FRIENDS
 
 GROUP_NAME = '中德人工智能协会'
-TRIGGER_WORDS = '我要进AI群'
-FIRST_REPLY = '您好%s，欢迎申请加入留德中国人工智能学会！请您先做个自我介绍，随后我会将您加入到群聊中。'
-INTRO_FAILED = '为了保证群聊质量，请您认真做一下自我介绍。'
-GOODBYE = '好的%s！我已把您加到群聊。请遵守群规，不要发广告及无关内容。谢谢！'
+TRIGGER_WORDS = ['我要进AI群', '我想进AI群', '拉进AI群']
+WARN_FWD = '注意，你的自我介绍将发送给其他群成员。'
+FIRST_REPLY = '您好%s，欢迎申请加入%s！请您先做个自我介绍，随后我会将您加入到群聊中。' + WARN_FWD
+INTRO_FAILED = '为了保证群聊质量，请您认真做一下自我介绍。' + WARN_FWD
+GOODBYE = '好的%s！我已向您发送了群聊邀请，请做确认。' \
+          '%s是中国驻德使馆教育处认可的组织。' \
+          '请保持良好的社区氛围，多发原创内容增进讨论, 严禁发广告及无关内容。' \
+          '如果您希望运维，投稿，赞助或合作，我们热烈欢迎！请与我私信联系，谢谢！' \
+          '最后，请牢记我们的域名 aichina.de 祝您在群里结识更多的朋友！'
+GROUP_INTRO = '欢迎来自即将加入的%s, ta是%s'
 
 
 class FriendStatus(Enum):
@@ -20,6 +26,13 @@ class FriendStatus(Enum):
 users = defaultdict(lambda: FriendStatus.UNKNOWN)
 
 
+def check_trigger_words(msg: str) -> bool:
+    for t in TRIGGER_WORDS:
+        if t.lower() in msg.lower():
+            return True
+    return False
+
+
 def check_self_intro(intro: str) -> bool:
     return len(intro) > 5
 
@@ -29,30 +42,35 @@ def send_group_invitation(msg) -> None:
     itchat.add_member_into_chatroom(chatroomUserName=chatroom_id, useInvitation=True, memberList=[msg.User])
 
 
+def intro_upcomming_member(user: str, intro: str) -> None:
+    chatroom_id = itchat.search_chatrooms(name=GROUP_NAME)[0].UserName
+    itchat.send_msg(msg=GROUP_INTRO % (user, intro), toUserName=chatroom_id)
+
+
 @itchat.msg_register(FRIENDS)
 def add_friends(msg):
     itchat.add_friend(**msg['Text'])
 
 
-
 @itchat.msg_register(TEXT, isFriendChat=True)
 def text_reply(msg):
-    if TRIGGER_WORDS.lower() in msg.Content.lower():
+    if check_trigger_words(msg.Content):
         if users[msg.FromUserName] == FriendStatus.INVITE_SENT:
             # the user sent trigger words again but the invitation already sent
             send_group_invitation(msg)
             return GOODBYE % msg.User.NickName
         else:
             users[msg.FromUserName] = FriendStatus.SELF_INTRO
-            return FIRST_REPLY % msg.User.NickName
+            return FIRST_REPLY % (msg.User.NickName, GROUP_NAME)
     if users[msg.FromUserName] == FriendStatus.SELF_INTRO:
         if check_self_intro(msg.Content):
             users[msg.FromUserName] = FriendStatus.INVITE_SENT
             send_group_invitation(msg)
+            intro_upcomming_member(msg.FromUserName, msg.Content)
             return GOODBYE % msg.User.NickName
         else:
             return INTRO_FAILED
 
 
-itchat.auto_login(hotReload=True)
+itchat.auto_login(hotReload=True, enableCmdQR=2)
 itchat.run()
